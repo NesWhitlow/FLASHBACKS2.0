8465e0a622a788ebc3dd7230033ffe4f23503186
Anessa Whitlow <90983417+NesWhitlow@users.noreply.github.com>
2022-10-01 14:11:32 -0500

    SecondMigration

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,5 +1,5 @@
 ﻿using FLASHBACKS.Models;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,16 +7,26 @@ namespace FLASHBACKS.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -24,14 +34,5 @@ namespace FLASHBACKS.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Marcus Fenix", street_address="342 Lancer Blvd.", city_address="Sera", state_address="UT", zip_address= 46461, phone_address=5465486897, email_address="grumblygear234@gmail.com"
-                },
-                new Customer { Id = 2, Name = "Donna Noble", street_address="Belmont Primary", city_address="Cardiff", state_address="UT", zip_address= 46462, phone_address=5485855753, email_address="dr.donna@live.com" }
-            };
-        }
     }
 }
